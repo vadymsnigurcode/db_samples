e22e28d406aad48499efa8e37e8f01ee5d354292
Vadym <vadym.snigur.code@gmail.com>
2022-09-04 18:46:38 +0300

    added sqlite file

---
+++ sqlite/main.sql
@@ -0,0 +1,8 @@
+CREATE TABLE t1(x INT CHECK( x>3 ));
+
+CREATE TABLE t(x INTEGER, y, z, PRIMARY KEY(x ASC));
+
+CREATE table person(ID INT PRIMARY KEY NOT NULL, name CHAR(255), surname CHAR(255), birth_date text)
+
+INSERT INTO person (ID, name, surname, birth_date)
+VALUES(1, 'pname','psurname',datetime('now', 'localtime'));
